93ca1b3f2e0411aa3395bdd5beae642b69f69a4d
ysevinchoy2197 <ysevinchoy2197@gmail.com>
2025-11-10 18:12:40 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+#2-misol
+matn = "salom dunyo"
+print(matn)
+print(f"birinchi xarf: {matn[0]}")
+print(f"oxirgi xarf: {matn[10]}")
+
+#3-misol
+matn = "Salom dunyo"
+print(matn)
+print(matn.upper())  #matn.lower
+
+#4-misol
+matn = "Salom dunyo"
+print(matn)
+print(matn.lower())
+
+#5-misol
+matn = "  Salom dunyo  "
+print(matn.strip())
+
+#6-misol
+matn = "hello world"
+uzunlik = len(matn)
+print(matn[uzunlik //2])
